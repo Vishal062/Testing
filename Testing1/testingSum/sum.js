b469a56ef20afb1a78935c1b32efb5915eebafc5
Vishal062 <vs9425348@gmail.com>
2021-12-08 23:24:48 +0530

    Sum assignment Done

--- Testing1/testingSum/sum.js
+++ Testing1/testingSum/sum.js
@@ -1,5 +1,9 @@
-function sumArguments(a, b) {
-    return a + b;
+function sumArguments(data) {
+    var total = 0;
+    for(let i=0; i<data.length; i++){
+        total += data[i]
+    }
+    return total;
 }
 
 module.exports = sumArguments;
--- Testing1/testingSum/sum.test.js
+++ Testing1/testingSum/sum.test.js
@@ -4,7 +4,18 @@ var sumNumber = require("./sum.js");
 //Describe function works here suit describes the suite of test cases enumerated by the "it" functions
 describe("Testing for Sum function", function () {
     test("It should return sum", function () {
-      expect(sumNumber(9,6)).toEqual(15);
+      expect(sumNumber([9,6])).toBe(15);
     });
 
+    test("It should return sum", function () {
+      expect(sumNumber([2, 4, 6, 8, 10])).toEqual(30);
+    });
+  
+    test("It should return sum", function () {
+      expect(sumNumber([1, 3, 5, 7, 9])).toEqual(25);
+    });
+
+    test("It should return sum", function () {
+      expect(sumNumber([1])).toEqual(1);
+    });
 });
